e1d1b566bf8609ebc51090b04487934056757a25
brianleao <brianleao@icloud.com>
2023-07-13 16:24:15 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //o classList é um objeto que vai ter algumas propriedades
+  // if(html.classList.contains('light')) {
+  //  html.classList.remove('light');
+  // }
+  // else {
+  //  html.classList.add('light');
+  // }
+  html.classList.toggle("light");
+
+  //pegar a tag img
+  const img = document.querySelector('#profile img');
+
+  //substituir a imagem
+  if(html.classList.contains('light')) {
+   //setAtribute serve para adicionar, ajustar, modificar um atributo, neste caso é o src. 
+    img.setAttribute("src", "./assets/brian-sun.png");
+    img.setAttribute("alt","Brian lindo, só que de óculos.");
+   }
+  else {
+    img.setAttribute("src", "./assets/brian.png");
+  }
+      
+}
