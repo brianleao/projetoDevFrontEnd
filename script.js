8561737d7dac6142a772c43841d4ad6895943f07
brianleao <brianleao@icloud.com>
2023-08-04 18:33:47 -0300

    alterei a descrição da imagem

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function toggleMode() {
-  const html = document.documentElement
+  const html = document.documentElement;
 
   //o classList é um objeto que vai ter algumas propriedades
   // if(html.classList.contains('light')) {
@@ -11,16 +11,14 @@ function toggleMode() {
   html.classList.toggle("light");
 
   //pegar a tag img
-  const img = document.querySelector('#profile img');
+  const img = document.querySelector("#profile img");
 
   //substituir a imagem
-  if(html.classList.contains('light')) {
-   //setAtribute serve para adicionar, ajustar, modificar um atributo, neste caso é o src. 
+  if (html.classList.contains("light")) {
+    //setAtribute serve para adicionar, ajustar, modificar um atributo, neste caso é o src.
     img.setAttribute("src", "./assets/brian-sun.png");
-    img.setAttribute("alt","Brian lindo, só que de óculos.");
-   }
-  else {
+    img.setAttribute("alt", "Brian, só que de óculos.");
+  } else {
     img.setAttribute("src", "./assets/brian.png");
   }
-      
 }
